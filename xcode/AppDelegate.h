348a417336e88aae9225006a4a5f2f5882663c75
Jusive <1345266022@qq.com>
2016-01-09 19:47:47 +0800

    Initial Commit

---
+++ xcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcode
+//
+//  Created by Jusive on 16/1/9.
+//  Copyright © 2016年 Jusive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xcode
+//
+//  Created by Jusive on 16/1/9.
+//  Copyright © 2016年 Jusive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
